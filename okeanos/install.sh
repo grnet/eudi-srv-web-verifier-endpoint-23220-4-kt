894bc1f0726ca2350001dcb88a92d714b86f91e3
Marios Fragkoulis <mfg@grnet.gr>
2024-09-24 13:37:56 +0000

    Generate a mock certificate in the installation script

--- okeanos/install.sh
+++ okeanos/install.sh
@@ -7,5 +7,11 @@ fi
 
 sudo apt-get update
 sudo apt-get install openjdk-21-jdk-headless gradle haproxy
+
+openssl ecparam -out ec_private_key.pem -name prime256v1 -genkey
+openssl req -new -key ec_private_key.pem -x509 -nodes -days 365 -subj "/CN=snf-895798.vm.okeanos.grnet.gr" -out grnet_cert.pem
+cat grnet_cert.pem ec_private_key.pem >> grnet_cert_key.pem
+sudo cp grnet_cert_key.pem /etc/ssl/certs/
+
 sudo cp haproxy.cfg /etc/haproxy/
 sudo service haproxy restart
